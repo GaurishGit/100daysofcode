4d8a825f3a889425f8531f23a6a73085948abda7
GaurishGit <gaurish0009@gmail.com>
2025-09-12 00:07:10 +0530

    Update question14.c

--- question14.c
+++ question14.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-    char z , a ,e , i , o , u ;
+    char z ;
 
     printf("Enter your letter\n");
 
     scanf("%c" , &z);
 
-    if ( z == a || e  || i ||  o || u )
+    if ( z == 'a' || z =='e'  || z =='i' ||z ==  'o' ||z == 'u' || z == 'A' || z =='E'  || z =='I' ||z ==  'O' ||z == 'U' )
  {printf("This is a vowel");}
 
     else {printf("This is a consonant"); }
